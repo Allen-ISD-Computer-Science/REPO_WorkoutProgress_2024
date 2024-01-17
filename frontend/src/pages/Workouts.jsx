da18021fd1fb22cd47dd42145282f6401b328129
Matthew-Roman <romanmatthew85@gmail.com>
2024-01-17 10:03:25 -0600

    Updated files -cleaned out unecessary packages -added temporarary image -modified workouts to display three sections -modified front pages css file

--- frontend/src/pages/Workouts.jsx
+++ frontend/src/pages/Workouts.jsx
@@ -4,12 +4,22 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Workouts() {
+    
     return (
 	<Container fluid className="vh-100">
-	    <Row className="text-center h-100">
-		<Col>Beginner</Col>
-		<Col>Intermediate</Col>
-		<Col>Expert</Col>
+	    <Row className="text-center">
+		<Col className="d-flex justify-content-center align-items-center">
+		    <img class="b-" src={"../images/pixelated_person.jpg"} alt="beginner" />
+		    <p>Beginner</p>
+		</Col>
+		<Col className="d-flex justify-content-center align-items-center">
+		    <img src={"../images/pixelated_person.jpg"} alt="intermediate" />
+		    <p>Intermediate</p>
+		</Col>
+		<Col className="d-flex justify-content-center align-items-center">
+		    <img src={"../images/pixelated_person.jpg"} alt="advanced" />
+		    <p>Advanced section</p>
+		</Col>
 	    </Row>
 	</Container>
     );
