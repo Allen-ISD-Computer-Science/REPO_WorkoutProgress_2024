f0e14aa391cba8b4164867b372459d3520872308
Matthew-Roman <romanmatthew85@gmail.com>
2024-01-22 23:17:04 -0600

    Created basic shell layout for the Workouts main page - It's split into three levels - Images will be replaced later, and links to subpages will be added once they are up. - removed useless css file

--- frontend/src/pages/Workouts.jsx
+++ frontend/src/pages/Workouts.jsx
@@ -1,27 +1,32 @@
-import '../styles/frontpages.css'
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+
+import '../styles/frontpages.css'
+import test_image from '../images/pixelman_still.png';
+
+
 
 function Workouts() {
     
     return (
-	<Container fluid className="vh-100">
-	    <Row className="text-center">
-		<Col className="d-flex justify-content-center align-items-center">
-		    <img class="b-" src={"../images/pixelated_person.jpg"} alt="beginner" />
-		    <p>Beginner</p>
+
+	    <Row className="text-center vh-100">
+		<Col className="w-33 d-flex justify-content-center align-items-center flex-column ">
+		    <img src={test_image} alt="1" class="workouts img-fluid"/>
+		    <Button href="./beginner" className="workouts_btn-style">Beginner</Button>
 		</Col>
-		<Col className="d-flex justify-content-center align-items-center">
-		    <img src={"../images/pixelated_person.jpg"} alt="intermediate" />
-		    <p>Intermediate</p>
+		<Col className="w-33 d-flex justify-content-center align-items-center flex-column ">
+		    <img src={test_image} alt="2" class="workouts img-fluid"/>
+		    <Button href="./intermediate" className="workouts_btn-style">Intermediate</Button>
 		</Col>
-		<Col className="d-flex justify-content-center align-items-center">
-		    <img src={"../images/pixelated_person.jpg"} alt="advanced" />
-		    <p>Advanced section</p>
+		<Col className="w-33 d-flex justify-content-center align-items-center flex-column advanced-bg-color">
+		    <img src={test_image} alt="3" class="workouts img-fluid"/>
+		    <Button href="./advanced" className="workouts_btn-style">Advanced</Button>
 		</Col>
-	    </Row>
-	</Container>
+	    </Row>	    
+
     );
 }
 
