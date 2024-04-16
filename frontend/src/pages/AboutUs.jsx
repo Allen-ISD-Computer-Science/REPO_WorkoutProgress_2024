716a9067172a660ba7c733312ce940cce265a018
myoram-langwang <Myoram Langwang>
2024-04-16 10:29:43 -0500

    NavBar Updated

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,43 +1,15 @@
 import React from "react";
 import Nav from '../components/Navbar.jsx';
 import {Container, Col, Row} from 'react-bootstrap';
+import '../styles/aboutus.css';
+
 function AboutUs() {
     
     return (
-	<div className="App">
+	<section>
 	    <Nav />
-	    <h1 className="h1">Feedback Form</h1>
-
-	    {!modal && (
-		<button
-		    classname="btn btn-red close-btn"
-		    onClick={() => setModal((value) => !value)}>
-			     Toogle Feedback
-	    </button>
-	    )}
-	    <div>
-
-
-		{modal && (
-		    <form className="feedback" onSubmit={HandleSubmit}>
-			<button className="close-btn-form" onClick={() => setModal((value) => !value)}>X</button>
-			
-		    <div className="m1-rem">
-			<input placeholder="Name" name="name" onChange={(e) => handleChange(e)}  />
-		    </div>
-		    <div>
-			<input placeholder="Email" name="email" onChange={(e) => handleChange(e)} />
-		    </div>
-		    <div className="m1-rem">>
-			<input placeholder="Feedback" name="feedback" onChange={(e) => handleChange(e)} />
-		    </div>
-			
-			<button className="btn btn-green" onClick={HandleSubmit}>Submit Feedback</button>
-		
-		    </form>
-		)}
-	    </div>
-	</div>
+	    </section>
     );
 }
 
+export default AboutUs;
