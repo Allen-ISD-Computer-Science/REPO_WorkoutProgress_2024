d1ddfab45c18c5eb9be616ec8c2e49acf5384b82
DanielComputerScience <elijah.williams@student.allenisd.org>
2024-05-07 09:26:31 -0500

    stuff

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,13 @@
-import React from 'react';
-import '../styles/Navbar.css';
-import logo from '../images/BeastLogo.png';
-
-
-function Navbar () {
-    
-    return(
-	<section>
-	<nav>
-	    <img src={logo} alt="" />
-	    <ul>
-		<li>Home</li>
-		<li>About Us</li>
-	    </ul>
-	</nav>
-	    </section>
-	
-
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import '../styles/frontpages.css';
+function NavbarComponent() {
+    const [showModal, setShowModal] = useState(false);
+    const [isLoggedIn] = useState(false); // Update based on actual login status
     const handleWorkoutsClick = () => {
         if (!isLoggedIn) {
             // If not logged in, show the modal
@@ -26,48 +17,48 @@ function Navbar () {
             window.location.href = './workouts';
         }
     };
-
     const handleModalClose = () => {
         setShowModal(false);
     };
-
     return (
         <>
             <Navbar className="align-self-center bg-primary">
-		<img src="favicon.ico" />
-		<Container className="d-flex justify-content-between text-white display-6">
-                    <Nav.Link href="./welcome">Welcome</Nav.Link>
-                    <Nav.Link href="./about_us">About Us</Nav.Link>
-                    <Nav.Link href="./diets">Diets</Nav.Link>
-		    <Nav.Link href="./supplements">Supplements</Nav.Link>
-                    <Nav.Link onClick={handleWorkoutsClick}>Workouts</Nav.Link>
-                    <Nav.Link href="./register">Register</Nav.Link>
-                    <Nav.Link href="./login">Login</Nav.Link>
-		    <Nav.Link href="./list">List</Nav.Link>
-		</Container>
-            </Navbar>
+		<Navbar.Brand className="text-white ">Bulk Like A Beast</Navbar.Brand>
+		<Container className="d-flex justify-content-between text-white">
+		    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
 
-            <Modal show={showModal} onHide={handleModalClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Login or Sign Up</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    Please login or sign up to access the Workouts page.
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleModalClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" href="./login">
-                        Login
-                    </Button>
-                    <Button variant="success" href="./register">
-                        Sign Up
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
+                    <Container className="d-flex justify-content-between text-white display-6">
+			<Nav.Link href="./welcome">Welcome</Nav.Link>
+			<Nav.Link href="./about_us">About Us</Nav.Link>
+			<Nav.Link href="./diets">Diets</Nav.Link>
+			<Nav.Link href="./supplements">Supplements</Nav.Link>
+			<Nav.Link onClick={handleWorkoutsClick}>Workouts</Nav.Link>
+			<Nav.Link href="./register">Register</Nav.Link>
+			<Nav.Link href="./login">Login</Nav.Link>
+			<Nav.Link href="./list">List</Nav.Link>
+		    </Container>
+		</Container>
+	    </Navbar>
+	    <Modal show={showModal} onHide={handleModalClose}>
+		<Modal.Header closeButton>
+		    <Modal.Title>Login or Sign Up</Modal.Title>
+		</Modal.Header>
+		<Modal.Body>
+		    Please login or sign up to access the Workouts page.
+		</Modal.Body>
+		<Modal.Footer>
+		    <Button variant="secondary" onClick={handleModalClose}>
+			Close
+		    </Button>
+		    <Button variant="primary" href="./login">
+			Login
+		    </Button>
+		    <Button variant="success" href="./register">
+			Sign Up
+		    </Button>
+		</Modal.Footer>
+	    </Modal>
+	</>	
     );
 }
-
-export default Navbar;
+export default NavbarComponent;
